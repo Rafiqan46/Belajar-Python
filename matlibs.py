d2c8188f03ea1ff92628be25985514b265146150
Rafiqan46 <baranrafiqan46@gmail.com>
2024-10-15 14:51:58 +0700

    Import Matlib 15/10/2024

---
+++ matlibs.py
@@ -0,0 +1,12 @@
+#Prompt
+nama = input("Nama: ")
+kegiatan = input("Kegiatan: ")
+kapan = input("Kapan: ")
+dimana = input("Dimana: ")
+
+#Cerita
+cerita=f"""Perkenalkan Nama Saya {nama}, {kapan} saya melakukan kegiatan {kegiatan},
+Di lokasi {dimana}
+"""
+
+print(cerita)
