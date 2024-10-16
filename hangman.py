ee93868badaa87eb2e8656d906199b245c4179b2
Rafiqan46 <baranrafiqan46@gmail.com>
2024-10-16 13:51:36 +0700

    modified:   hangman.py 	new file:   src/__pycache__/kata.cpython-312.pyc 	new file:   src/__pycache__/kataKata.cpython-312.pyc 	deleted:    src/kata.py 	new file:   src/kataKata.py

--- hangman.py
+++ hangman.py
@@ -1,6 +1,31 @@
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src/'))
 import random
-from kata import kata
+from kataKata import kataKata
 
-print(kata)
+def get_valid_word(kataKata):
+    kata = random.choice(kataKata)
+    while '-' in kata or ' ' in kata:
+        kata = random.choice(kataKata)
+
+def hangman():
+    kata = get_valid_word(kataKata)
+    hurufKata = set(kata)
+    alphabet = set(string.ascii_uppercase)
+    hurufDigunakan = set()
+
+    user_huruf = input('Tebak Sebuah Huruf: ').upper
+    if user_huruf in alphabet - hurufDigunakan:
+        hurufDigunakan.add(user_huruf)
+        if user_huruf in hurufKata:
+            hurufKata.remove(user_huruf)
+
+    elif user_huruf in hurufDigunakan:
+        print("Kamu Sudah Mengunnakan Karakter itu, Coba Lagi")
+
+    else:
+        print('Invalid Character, Coba Lagi!!')
+
+user_input = input("ketik Sesuatu: ")
+print(user_input)
+
